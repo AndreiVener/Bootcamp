3b718f1cab568fc5ea4320bbb75f090b5a289f73
Andrei Vener <Andrei.Vener@softwire.com>
2022-06-27 13:41:10 +0100

    FizzBuzz Part 1

--- FizzBuzz/FizzBuzz/Program.cs
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace FizzBuzz
+﻿using System;
+
+namespace FizzBuzz
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            for (int k = 1; k <= 100; k++)
+            {
+                if (k % 15 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (k % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }else if (k % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }else
+                {
+                    Console.WriteLine(k);
+                }
+                
+            }
         }
     }
 }
